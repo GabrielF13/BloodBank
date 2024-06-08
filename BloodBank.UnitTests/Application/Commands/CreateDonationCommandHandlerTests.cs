619a62def35faaa66c32deb94cc12e2493133b98
Gabriel Ferreira <gabrielferreiracunha@gmail.com>
2024-06-07 22:52:55 -0300

    ADD: add tests

---
+++ BloodBank.UnitTests/Application/Commands/CreateDonationCommandHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using BloodBank.Application.Abstractions;
+using BloodBank.Application.Commands.CreateDonate;
+using BloodBank.Core.Entities;
+using BloodBank.Core.Repositories;
+using BloodBank.Infrastructure.Persistence;
+using Moq;
+using System.Diagnostics.Eventing.Reader;
+
+namespace BloodBank.UnitTests.Application.Commands
+{
+    public class CreateDonationCommandHandlerTests
+    {
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnDonateId()
+        {
+            var unitOfWork = new Mock<IUnitOfWork>();
+            var donateRepositoryMock = new Mock<IDonateRepository>();
+
+            unitOfWork.SetupGet(uow => uow.Donates).Returns(donateRepositoryMock.Object);
+
+            var createDonateCommand = new CreateDonateCommand
+            {
+                QuantityMl = 450,
+                DonorPersonId = 5
+            };
+
+            var createDonateCommandHandler = new CreateDonateCommandHandler(unitOfWork.Object);
+
+            var id = await createDonateCommandHandler.Handle(createDonateCommand, new CancellationToken());
+
+
+            Assert.True(id.IsSuccess);
+
+            unitOfWork.Verify(x => x.CompleteAsync(), Times.Once);
+        }
+    }
+}
---
+++ BloodBank.UnitTests/Core/Entities/DonationTests.cs
@@ -0,0 +1,19 @@
+﻿using BloodBank.Core.Entities;
+
+namespace BloodBank.UnitTests.Core.Entities
+{
+    public class DonationTests
+    {
+        [Fact]
+        public void TestIfDonationWorks()
+        {
+            var donate = new Donation(5, 450);
+
+            Assert.NotNull(donate);
+
+            donate.Update(10);
+
+            Assert.Equal(460, donate.QuantityMl);
+        }
+    }
+}
